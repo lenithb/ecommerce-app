31b83891c75361fdca3b2dd76a9abf9299a6b72e
Lenith Block <Lenithblock@gmail.com>
2026-09-08 11:14:47 -0300

    fix: useProduct ahora con return para no tener lógica duplicada

--- src/hooks/useProduct.ts
+++ src/hooks/useProduct.ts
@@ -5,6 +5,5 @@ import type { Product } from "../interfaces/Product";
 // hook para obtener un solo producto por su id (para ProductDetail)
 export function useProduct(id: string | undefined): Product | undefined {
   const ctx = useContext(ProductContext);
-  // buscar y devolver el producto cuyo id coincida con `id`
-  return ctx?.products.find((p) => p.id === id);
+  return ctx?.getProductById(id);
 }
